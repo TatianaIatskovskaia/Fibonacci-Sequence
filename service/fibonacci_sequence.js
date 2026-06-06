b6d3c6f91def0dcc005ca2352300d4599ae77608
Tatiana <t.ya14091984@gmail.com>
2026-06-06 20:02:53 +0300

    Refactor Fibonacci sequence implementation to use iterator pattern and update tests accordingly.

--- service/fibonacci_sequence.js
+++ service/fibonacci_sequence.js
@@ -1,12 +1,23 @@
-const fibonacci = (n) => {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-    const result = [0, 1];
-    while (n > 2) {
-        result.push(result[result.length - 1] + result[result.length - 2]);
-        n--;
+function Fibonacci(n) {
+    this.n = n;
+    this[Symbol.iterator] = function() {
+        let counter = 0;
+        let prev = 0;
+        let current = 1;
+        return {
+            num: n,
+            next: function() {
+                if (this.num <= 0) return {value: undefined, done: true};
+                if (counter < this.num) {
+                    counter++;
+                    [prev, current] = [current, prev + current];
+                    return {value: prev, done: false};
+                } else {
+                    return {done: true}
+                }
+            }
+        }
     }
-    return result;
-};
+}
 
-export {fibonacci};
+export {Fibonacci};
--- test/fibonacci_sequence.test.js
+++ test/fibonacci_sequence.test.js
@@ -1,20 +1,20 @@
 import {test, expect, describe} from "@jest/globals";
-import {fibonacci} from "../service/fibonacci_sequence.js";
+import {Fibonacci} from "../service/fibonacci_sequence.js";
 
 describe("Fibonacci Sequence", () => {
     test('should return the Fibonacci sequence', () => {
-        expect(fibonacci(5)).toEqual([0, 1, 1, 2, 3]);
-        expect(fibonacci(10)).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
-        expect(fibonacci(0)).toEqual([]);
-        expect(fibonacci(-1)).toEqual([]);
-        expect(fibonacci(1)).toEqual([0]);
-        expect(fibonacci(2)).toEqual([0, 1]);
+        expect([...new Fibonacci(5)]).toEqual([1, 1, 2, 3, 5]);
+        expect([...new Fibonacci(10)]).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
+        expect([...new Fibonacci(0)]).toEqual([]);
+        expect([...new Fibonacci(-1)]).toEqual([]);
+        expect([...new Fibonacci(1)]).toEqual([1]);
+        expect([...new Fibonacci(2)]).toEqual([1, 1]);
     })
 })
 describe("The sum of Fibonacci numbers", () => {
     test("The sum of the first n Fibonacci numbers", () => {
         const n = 10;
-        const result = fibonacci(n).reduce((a, b) => a + b);
-        expect(result).toEqual(88);
+        const result = ([...new Fibonacci(n)]).reduce((a, b) => a + b, 0);
+        expect(result).toEqual(143);
     });
 });
