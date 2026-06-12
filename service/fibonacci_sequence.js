12bde1eff1f6ad9c912ccaca98404371a4c5a264
Tatiana <t.ya14091984@gmail.com>
2026-06-12 13:22:47 +0300

    Refactor Fibonacci sequence to use generator function for cleaner iterator implementation. Update dependencies in package.json.

--- service/fibonacci_sequence.js
+++ service/fibonacci_sequence.js
@@ -1,23 +1,36 @@
-function Fibonacci(n) {
-    this.n = n;
-    this[Symbol.iterator] = function() {
-        let counter = 0;
-        let prev = 0;
+export function Fibonacci(n) {
+    this[Symbol.iterator] = function* () {
+        let prev = 1;
         let current = 1;
-        return {
-            num: n,
-            next: function() {
-                if (this.num <= 0) return {value: undefined, done: true};
-                if (counter < this.num) {
-                    counter++;
-                    [prev, current] = [current, prev + current];
-                    return {value: prev, done: false};
-                } else {
-                    return {done: true}
-                }
-            }
+        for (let i = 0; i < n; i++) {
+            yield prev;
+            [prev, current] = [current, prev + current];
         }
     }
 }
 
-export {Fibonacci};
+
+
+// function Fibonacci(n) {
+//     this.n = n;
+//     this[Symbol.iterator] = function() {
+//         let counter = 0;
+//         let prev = 0;
+//         let current = 1;
+//         return {
+//             num: n,
+//             next: function() {
+//                 if (this.num <= 0) return {value: undefined, done: true};
+//                 if (counter < this.num) {
+//                     counter++;
+//                     [prev, current] = [current, prev + current];
+//                     return {value: prev, done: false};
+//                 } else {
+//                     return {done: true}
+//                 }
+//             }
+//         }
+//     }
+// }
+//
+// export {Fibonacci};
